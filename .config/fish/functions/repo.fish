a59b0c6d0b10fde62f3b0e1c006d232b5a62a80e
Maxine Krebs <krebs.maxine@gmail.com>
2024-02-16 22:17:02 -0500

    Fix repo command for new naming conventions

--- .config/fish/functions/repo.fish
+++ .config/fish/functions/repo.fish
@@ -1,5 +1,5 @@
 function repo
-  set repo (find -H $HOME/code -maxdepth 2 -type d -or -type l | fzf --height 25% --reverse --border --header='Repositories:')
+  set repo (find -H $HOME/repos -maxdepth 2 -type d -or -type l | fzf --height 25% --reverse --border --header='Repositories:')
   if [ $repo != '' ]
     cd $repo && pwd && ls
   end
